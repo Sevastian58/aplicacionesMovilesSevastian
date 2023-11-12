0fd5f4ef397019eb07ce46c1798692181d780d19
Sevastian58 <111455344+Sevastian58@users.noreply.github.com>
2023-11-12 11:53:47 -0500

    Update AplicacionesMovilesIiAntonyApplication.java
    si no funciona borra

--- src/main/java/com/cibertec/aplicacionesmovilesiiantony/AplicacionesMovilesIiAntonyApplication.java
+++ src/main/java/com/cibertec/aplicacionesmovilesiiantony/AplicacionesMovilesIiAntonyApplication.java
@@ -3,14 +3,21 @@ package com.cibertec.aplicacionesmovilesiiantony;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.cibertec.aplicacionesmovilesiiantony.model")
 
-public class AplicacionesMovilesIiAntonyApplication {
+public class AplicacionesMovilesIiAntonyApplication extends WebMvcConfigurer{
 
     public static void main(String[] args) {
         SpringApplication.run(AplicacionesMovilesIiAntonyApplication.class, args);
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 
 }
